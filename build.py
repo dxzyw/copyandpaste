c0bcaa687ddbdb850aff463e11f72a134a64771f
unknown <17621764533@163.com>
2024-11-04 16:40:37 +0800

    1104

--- build.py
+++ build.py
@@ -4,7 +4,7 @@ import re
 import urllib.parse
 
 def fetch_ci_time(file_path):
-    url = f"https://api.github.com/repos/dxzyw/higithub/commits?path={file_path}&page=1&per_page=1"
+    url = f"https://api.github.com/repos/dxzyw/copyandpaste/commits?path={file_path}&page=1&per_page=1"
     response = httpx.get(url)
     ci_time = response.json()[0]["commit"]["committer"]["date"].split("T")[0]
     print(ci_time)
@@ -12,7 +12,7 @@ def fetch_ci_time(file_path):
 
 if __name__ == "__main__":
     with open('README.md', 'w') as readme_file, open('RECENT.md', 'w') as recent_file:
-        readme_file.write("# 开源周刊\n\n> 记录工程师 的不枯燥生活，欢迎订阅，也欢迎 [推荐](https://github.com/dxzyw/higithub/discussions/22) 你的好东西，Fork 自用可见 [开发文档](https://github.com/dxzyw/weekly/blob/main/Deploy.md)，期待你玩得开心~\n\n")
+        readme_file.write("# 开源周刊\n\n> 记录工程师 的不枯燥生活，欢迎订阅，也欢迎 [推荐](https://github.com/dxzyw/copyandpaste/discussions/22) 你的好东西，Fork 自用可见 [开发文档](https://github.com/dxzyw/weekly/blob/main/Deploy.md)，期待你玩得开心~\n\n")
 
         for root, dirs, filenames in os.walk('./src/pages/posts'):
             filenames = sorted(filenames, key=lambda x: float(re.findall(r"(\d+)", x)[0]), reverse=False)
@@ -22,7 +22,7 @@ if __name__ == "__main__":
             if name.endswith('.md'):
                 file_path = urllib.parse.quote(name)
                 old_title = name.split('.md')[0]
-                url = f'https://higithub.herotops.xyz/posts/{old_title}'
+                url = f'https://copyandpaste.herotops.xyz/posts/{old_title}'
                 title = f'第 {old_title.split("-")[0]} 期 - {old_title.split("-")[1]}'
                 readme_md = f'* [{title}]({url})\n'
                 num = int(old_title.split('-')[0])
